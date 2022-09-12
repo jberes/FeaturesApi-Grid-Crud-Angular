56cc1698983eb32788688b8d4076280652d1be57
Jason Beres <jasonberes@gmail.com>
2022-09-12 10:13:18 -0400

    updated CRUD code

--- src/app/view1/view1.component.ts
+++ src/app/view1/view1.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { IGridEditDoneEventArgs, IgxGridComponent, IgxSnackbarComponent, IRowDataEventArgs } from 'igniteui-angular';
+import { Feature } from '../models/features-model';
 import { FeaturesService } from '../services/features.service';
 
 @Component({
@@ -6,8 +8,11 @@ import { FeaturesService } from '../services/features.service';
   templateUrl: './view1.component.html',
   styleUrls: ['./view1.component.scss']
 })
+
 export class View1Component implements OnInit {
-  public featuresFeatures: any = null;
+  public featuresFeatures!: Feature[];
+  @ViewChild('grid', { static: true }) public grid: IgxGridComponent | undefined;
+  @ViewChild('snackbar', { static: true }) public snackbar: IgxSnackbarComponent | undefined;
 
   constructor(
     private featuresService: FeaturesService,
@@ -16,5 +21,23 @@ export class View1Component implements OnInit {
   ngOnInit() {
     // depending on implementation, data subscriptions might need to be unsubbed later
     this.featuresService.getFeatures().subscribe(data => this.featuresFeatures = data);
+  }
+
+  async rowAdd(event: IRowDataEventArgs) {
+    var newFeature = await this.featuresService.addFeature(event.data);
+    this.grid?.updateRow(newFeature, event.data.featureRequestId);
+    this.snackbar?.open("Row Added");
+  }
+
+  async rowEdit(event: IGridEditDoneEventArgs){
+    if (!event.isAddRow){
+      await this.featuresService.updateFeature(event.newValue)
+      this.snackbar?.open("Row Updated");
+    }
+  }
+
+  async rowDelete(event: IRowDataEventArgs){
+    await this.featuresService.deleteFeature(event.data);
+    this.snackbar?.open("Row Deleted");
   }
 }
